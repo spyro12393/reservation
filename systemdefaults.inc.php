2091c91386a9e0dab7ce604748dc8d0220f343d2
System Administrator <root@Mansions-MacBook-Pro.local>
2018-09-24 23:57:06 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: System Administrator <root@Mansions-MacBook-Pro.local>
    
     On branch mansion
     Your branch is up to date with 'origin/mansion'.
    
     Changes to be committed:
    	modified:   mincals.inc
    	modified:   systemdefaults.inc.php

--- systemdefaults.inc.php
+++ systemdefaults.inc.php
@@ -103,7 +103,7 @@ $mrbs_admin_email = "admin_email@your.org";
 // The company name is mandatory.   It is used in the header and also for email notifications.
 // The company logo, additional information and URL are all optional.
 
-$mrbs_company = "Your Company";   // This line must always be uncommented ($mrbs_company is used in various places)
+$mrbs_company = "中央大學-課務組";   // This line must always be uncommented ($mrbs_company is used in various places)
 
 // Uncomment this next line to use a logo instead of text for your organisation in the header
 //$mrbs_company_logo = "your_logo.gif";    // name of your logo file.   This example assumes it is in the MRBS directory
